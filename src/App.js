a5ac8b461d900a7fd194c877c43dd552a1eb01d4
yfsoftcom <yfsoftcom@126.com>
2020-09-02 17:47:00 +0800

    add react-router

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React from 'react';
 import './App.css';
 
 import { Nav, Navbar, Container, Form, Table, Button } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Project from './project.js';
 
 function App() {
   return (
-    <>
+    <Router>
     <Container fluid={true}>
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="#home">FPM-IOT-ADMIN</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#dashboard">Dashboard</Nav.Link>
-          <Nav.Link href="#mqtt">MQTT</Nav.Link>
-          <Nav.Link href="#project">Project</Nav.Link>
-          <Nav.Link href="#setting">Setting</Nav.Link>
+          <Nav.Link href="#"><Link to="/">Dashboard</Link></Nav.Link>
+          <Nav.Link href="#"><Link to="/mqtt">MQTT</Link></Nav.Link>
+          <Nav.Link href="#"><Link to="/project">Project</Link></Nav.Link>
+          <Nav.Link href="#"><Link to="/setting">Setting</Link></Nav.Link>
           
         </Nav>
         {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
@@ -32,9 +38,18 @@ function App() {
       </Navbar.Collapse>
     </Navbar>
     </Container>
-      
-    <Project />     
-    </>
+    <Switch>
+      <Route exact path="/">
+        <Project />
+      </Route>
+      <Route path="/mqtt">
+        <Project />
+      </Route>
+      <Route path="/project">
+        <Project />
+      </Route>
+    </Switch>
+    </Router>
   );
 }
 
